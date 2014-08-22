d9919aef9f20597b6e6f13e75afcc412af7c41b2
ETHEL NG <ethel.ng@gmail.com>
2014-08-22 13:26:17 +0100

    Added airport capacity and inability to land planes if that capacity has been reached.

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,15 +1,28 @@
 class Airport
 
+	DEFAULT_CAPACITY = 50
+
+	attr_writer :capacity
+
+	def capacity
+		@capacity ||= DEFAULT_CAPACITY
+	end
+
 	def planes
 		@planes ||= []
 	end
 
 	def land(plane)
+		raise "The airport has no more space!" if full?
 		planes << plane
 	end
 
 	def take_off(plane)
 		planes.delete(plane)
+	end
+
+	def full?
+		planes.count == capacity
 	end
 
 end
